b4e282c2320e59539b957077ccea0191c0da856f
Rodrigo G. Oliveira <mjgargani@gmail.com>
2022-12-15 17:43:04 -0300

    feat(ex9): adiciona logica quiz, resultados e reset

--- exercises/ex9/script.js
+++ exercises/ex9/script.js
@@ -1,6 +1,8 @@
 let startTime;
 let timer;
-let quizState = 0 // 0: Tela inicial; 1: Quiz rodando; 2: Resultados
+let quizState = 0; // 0: Tela inicial; 1: Quiz rodando; 2: Resultados
+let currentQuestion = 0;
+let currentAnswer;
 
 const results = {
   correct: 0,
@@ -18,6 +20,10 @@ const elements = {
     question: {
       index: document.getElementById("label-question-index"),
       description: document.getElementById("label-question-description")
+    },
+    results: {
+      correct: document.getElementById("label-quiz-corrects"),
+      incorrect: document.getElementById("label-quiz-incorrects")
     }
   },
   form: {
@@ -30,6 +36,33 @@ const elements = {
   }
 }
 
+function renderQuestion() {
+  const current = questions[currentQuestion];
+  console.log(current);
+
+  elements.label.question.index.textContent = currentQuestion + 1;
+  elements.label.question.description.textContent = current.question;
+  elements.form.questionAlternatives.replaceChildren();
+
+  for (let i = 0; i < current.alternatives.length; i++) {
+    const input = document.createElement("input");
+    const label = document.createElement("label");
+    const br = document.createElement("br");
+
+    input.setAttribute("type", "radio");
+    input.setAttribute("name", "alternative-item");
+    input.setAttribute("value", i);
+
+    label.textContent = current.alternatives[i];
+
+    elements.form.questionAlternatives.appendChild(input);
+    elements.form.questionAlternatives.appendChild(label);
+    elements.form.questionAlternatives.appendChild(br);
+  }
+
+  currentAnswer = current.correct;
+}
+
 function renderTimer(ms) {
   const min = Math.floor(ms / 60000);
   const sec = ((ms % 60000) / 1000).toFixed(0);
@@ -40,8 +73,31 @@ function renderTimer(ms) {
 function showResults() {
   quizState = 2;
 
+  clearInterval(timer);
   elements.div.quiz.classList.add("hide");
   elements.div.quizResults.classList.remove("hide");
+
+  elements.label.results.correct.textContent = results.correct;
+  elements.label.results.incorrect.textContent = results.incorrect;
+}
+
+function validateAnswer(event) {
+  event.preventDefault();
+  const selected = document.querySelector('input[name="alternative-item"]:checked').value;
+
+  if(Number(selected) === currentAnswer){
+    results.correct += 1;
+  } else {
+    results.incorrect += 1;
+  }
+
+  currentQuestion += 1;
+
+  if(currentQuestion < questions.length) {
+    renderQuestion();
+  } else {
+    changeQuizState(2);
+  }
 }
 
 function validateTimer(ms) {
@@ -49,9 +105,8 @@ function validateTimer(ms) {
   const timeLeft = ms - difference;
   
   renderTimer(timeLeft);
-  
+
   if(difference > ms){
-    clearInterval(timer);
     changeQuizState(2);
   }
 }
@@ -68,6 +123,8 @@ function showQuiz() {
   elements.div.quiz.classList.remove("hide");
   
   startTimer(600000);
+
+  renderQuestion();
 }
 
 function showHome() {
@@ -75,6 +132,10 @@ function showHome() {
 
   elements.div.quizResults.classList.add("hide");
   elements.div.startQuiz.classList.remove("hide");
+
+  results.correct = 0;
+  results.incorrect = 0;
+  currentQuestion = 0;
 }
 
 function changeQuizState(stateNum) {
@@ -93,3 +154,5 @@ function changeQuizState(stateNum) {
 
 elements.btn.startQuiz.addEventListener("click", () => changeQuizState(1));
 elements.btn.restartQuiz.addEventListener("click", () => changeQuizState(0));
+
+elements.form.question.addEventListener("submit", validateAnswer);
